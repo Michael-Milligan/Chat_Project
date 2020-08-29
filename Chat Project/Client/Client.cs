c2fb24d240b6fe307054e6aee3a991703677c279
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-08-29 17:51:55 +0300

    Need to make MVVM

---
+++ Chat Project/Client/Client.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class Client
+    {
+        static string _UserName;
+        static int _Port = 8888;
+        static string _IP = "127.0.0.1";
+        static NetworkStream _Stream;
+        static TcpClient _Client;
+
+        static void Main()
+        {
+            IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Parse(_IP), _Port);
+
+            _Client = new TcpClient();
+            Console.Write("Type your name here: ");
+            _UserName = Console.ReadLine();
+            try
+            {
+                _Client.Connect(LocalEndPoint);
+                _Stream = _Client.GetStream();
+                NetworkStream Stream = _Client.GetStream();
+                _Stream.Write(Encoding.UTF8.GetBytes(_UserName));
+
+                Thread ReceiveThread = new Thread(ReceiveMessages);
+                ReceiveThread.Start();
+                Console.WriteLine($"Welcome, {_UserName}");
+                SendMessages();
+            }
+            catch(SocketException Exception)
+            {
+                Console.WriteLine("The connection is lost.");
+            }
+            finally
+            {
+                if (_Client != null) _Client.Close();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Disconnect()
+        {
+            if (_Stream != null)
+                _Stream.Close();//отключение потока
+            if (_Client != null)
+                _Client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+
+        public static void ReceiveMessages()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] Buffer = new byte[1024];
+                    StringBuilder Message = new StringBuilder();
+                    do
+                    {
+                        int Length = _Stream.Read(Buffer, 0, Buffer.Length);
+                        Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
+                    }
+                    while (_Stream.DataAvailable);
+                    Console.Beep();
+                    Console.Write(Message.Replace("\n\n", "\n"));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The connection is lost.");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+            
+        }
+
+        public static void SendMessages()
+        {
+            Console.WriteLine("\nEnter your message: ");
+            while (true)
+            {
+                Console.Write("\rYou: ");
+                byte[] Buffer = new byte[1024];
+                Buffer = Encoding.UTF8.GetBytes(Console.ReadLine());
+                _Stream.Write(Buffer);
+
+            }
+        }
+    }
+}
---
+++ Chat Project/Server/ServerObject.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class ClientObject
+    {
+        internal NetworkStream _Stream;
+        TcpClient _Client;
+        ServerObject _Server;
+
+            internal string _ID;
+            string _UserName;
+
+            public ClientObject(TcpClient Client, ServerObject Server)
+            {
+                _Server = Server ?? throw new ArgumentNullException(nameof(Server));
+                _Stream = Client.GetStream();
+                _Client = Client ?? throw new ArgumentNullException(nameof(Client));
+                _ID = Guid.NewGuid().ToString();
+                _Server.AddConnection(this);
+            }
+
+            public void Process()
+            {
+                try
+                {
+                    _UserName = GetMessage()[..^1];
+                    var Message = _UserName + " has entered the chat";
+                    Console.Beep();
+                    Console.WriteLine(Message);
+                    _Server.BroadcastMessage($"\r{Message}\nYou: ", _ID);
+                    while (true)
+                    {
+                        Message = $"\r{_UserName}: {GetMessage()}You: ";
+                        _Server.BroadcastMessage(Message, _ID);
+                    }
+                }
+                catch
+                {
+                    var Message = _UserName + " has left the chat";
+                    Console.Beep();
+                    Console.WriteLine(Message);
+                    _Server.BroadcastMessage($"\r{Message}\nYou: ", _ID);
+                }
+                finally
+                {
+                    _Server.RemoveConnection(_ID);
+                    Close();
+                }
+            }
+
+            public string GetMessage()
+            {
+                StringBuilder Message = new StringBuilder();
+                byte[] Buffer = new byte[1024];
+
+                do
+                {
+                    int Length = _Stream.Read(Buffer);
+                    Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
+                }
+                while (_Stream.DataAvailable);
+                Message.Append("\n");
+                return Message.ToString();
+            }
+
+            internal void Close()
+            {
+                if (_Stream != null)
+                    _Stream.Close();
+                if (_Client != null)
+                    _Client.Close();
+            }
+    }
+
+    public class ServerObject
+    {
+        internal TcpListener _Server;
+        List<ClientObject> _Clients = new List<ClientObject>();
+
+        public void AddConnection(ClientObject Client)
+        {
+            _Clients.Add(Client);
+        }
+
+        public void RemoveConnection(string ID)
+        {
+            try { _Clients.Remove(_Clients.FirstOrDefault(item => item._ID == ID)); }
+            catch (Exception) { }
+
+        }
+
+        public void BroadcastMessage(string Message, string ID)
+        {
+            foreach (var Client in _Clients.Where(Client => Client._ID != ID))
+            {
+                Client._Stream.Write(Encoding.UTF8.GetBytes(Message, 0, Message.Length));
+            }
+        }
+
+        public void Disconnect()
+        {
+            foreach (var Client in _Clients)
+            {
+                Client.Close();
+            }
+
+            _Server.Stop();
+
+            Environment.Exit(0);
+
+        }
+
+        public void Listen()
+        {
+            _Server = new TcpListener(IPAddress.Any, 8888);
+
+            try
+            {
+                Console.WriteLine("Waiting for connections...");
+                Console.WriteLine("The connection dates and messages:");
+                _Server.Start(10);
+
+                while (true)
+                {
+                    TcpClient Client = _Server.AcceptTcpClient();
+                    ClientObject Object = new ClientObject(Client, this);
+                    Thread NewThread = new Thread(Object.Process);
+                    NewThread.Start();
+                }
+            }
+            catch (SocketException Exception)
+            {
+                Console.WriteLine($"The error: {Exception.Message}\nThe Stacktrace: {Exception.StackTrace}\nThe code of error: {Exception.SocketErrorCode}");
+                Disconnect();
+            }
+        }
+    }
+}
